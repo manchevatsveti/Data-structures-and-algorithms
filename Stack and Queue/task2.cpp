64a1e9460093d02dfa270b358f518aeef8e00243
Tsvetelina Mancheva <72023208+manchevatsveti@users.noreply.github.com>
2024-09-06 14:54:17 +0300

    Update task2.cpp

--- Stack and Queue/task2.cpp
+++ Stack and Queue/task2.cpp
@@ -1,51 +1,53 @@
-#include <map>
-#include <set>
-#include <list>
 #include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
 #include <cstdio>
-#include <limits>
 #include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
-
+#include <bits/stdc++.h> 
 using namespace std;
+
+
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    string s1,s2;
-    cin>>s1>>s2;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
     
-    size_t maxD=0;
-    size_t curr =0;
+    vector<int> arr;
+    arr.resize(n);
     
-    size_t size1 = s1.size();
-    size_t size2 = s2.size();
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     
-    for(size_t i=0;i<size1;i++){
-        size_t id =i;
-        for(size_t j=0;j<size2 && id<size1;j++){
-            if(s1[id]==s2[j]){
-                curr++;
-                maxD = max(maxD,curr);
+    stack<int> s;
+    int result=0;
+    
+    for(int i=0;i<n;i++){
+        if(s.empty() || arr[i]>=arr[s.top()]){
+            s.push(i);
+        }else{
+            int temp=s.top();
+            s.pop();
+            
+            if(s.empty()){
+                result=max(result,arr[temp]*i);
             }else{
-                curr=0;
+                result=max(result,arr[temp]*(i-s.top()-1));
             }
-            id++;
+            i--;
         }
     }
     
-    cout<<maxD;
+    while(!s.empty()){
+        int temp=s.top();
+        s.pop();
+        
+        if(s.empty()){
+            result=max(result,arr[temp]*n);
+        }else{
+            result=max(result,arr[temp]*(n-s.top()-1));
+        }
+    }
+    cout<<result;
     return 0;
 }
